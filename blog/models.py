b54c61315bebbc52d7619f433b31b9bc452eeeb6
Marek <marekdrag@gmail.com>
2023-02-08 13:42:08 +0100

    Modified Post model and add view to filter newest on page
    In the Post model, a field category, summary, image for summary and two fields for images for the main post was added.
    In settings.py MEDIA_URL and MEDIA_ROOT for displaying images on the page added to the database was added.
    In post_list.html, the loading of external files was changed to loading
    from a static location.
    In post_list.html the static articles to dynamic ones with a Django template variables and tags was changed.

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,12 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    summary_text = models.CharField(max_length=300)
+    summary_image = models.ImageField(upload_to='', default=None)
+    text_image_1 = models.ImageField(upload_to='', default=None)
+    text_image_2 = models.ImageField(upload_to='', default=None)
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
