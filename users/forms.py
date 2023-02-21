e6247b9f9b9ad6debc0ec8eb020d6a5111eff004
Marek <marekdrag@gmail.com>
2023-02-21 18:58:36 +0100

    Added User APP to adding and registering new users
    Registration form with built-in features of django and crispi apps was
    added. The login site is only displayed.

---
+++ users/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    sex = forms.ChoiceField(choices=(("1", "Female"),
+                                     ("2", "Male"),
+                                     ))
+
+    class Meta:
+        model = User
+        fields = ("username", "sex", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
---
+++ users/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+
+
+def dashboard(request):
+    return render(request, "users/dashboard.html")
+
+
+def login_go(request):
+    return render(request, "users/login.html")
+
+
+def register_request(request):
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("user/")
+
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+
+    return render(request=request, template_name="users/register.html", context={"register_form": form})
