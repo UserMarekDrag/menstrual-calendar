afafd4497005ba5f30c1ba74d4a37574d69b35a1
Marek <marekdrag@gmail.com>
2023-02-24 19:23:11 +0100

    Added Profile model with one to one with User model
    Data about date of birth and gender in Profile model was added.
    In base.html, a Profile link was added in the navigation bar.
    In register_done.html link to login was changed.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER_CHOICE = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+    ]
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICE, default=FEMALE)
+
+    def __str__(self):
+        return 'Profil użytkownika {}.'.format(self.user.username)
--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 
 @login_required
@@ -20,7 +21,11 @@ def register(request):
             new_user.set_password(
                 user_form.cleaned_data['password'])
             new_user.save()
-            return render(request, 'users/register_done.html', {'new_user': new_user})
+            profile = Profile.objects.create(user=new_user)
+            return render(request, 'users/register_done.html',
+                          {'new_user': new_user},
+                          {'profile': profile},
+                          )
 
     else:
         user_form = UserRegistrationForm()
@@ -51,3 +56,20 @@ def user_login(request):
         form = LoginForm()
 
     return render(request, 'users/login.html', {'form_login': form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return render(request, 'users/edit_done.html', {'user_form': user_form, 'profile_form': profile_form})
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(request, 'users/edit.html', {'user_form': user_form, 'profile_form': profile_form})
